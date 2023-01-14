857b220a424a373e22834acc0036779a537f4f14
Andriamisa Nayann <99319983+nayannou@users.noreply.github.com>
2023-01-14 17:37:59 +0100

    Add files via upload

--- Model/Model.php
+++ Model/Model.php
@@ -29,76 +29,190 @@ class Model {
  	* paramêtres : identifiant et mot de passe
  	* return bool
  	*/
-	public function connexion($identifiant, $motDePasse){
-    	$reqette = $this->bd->prepare("SELECT id_utilisateur, mdp from utilisateur where identifiant = ':identifiant'");
-    	$reqette->bindValue(':identifiant', $identifiant);
-    	$reqette->execute();
-    	$tab = $reqette->fetch(PDO::FETCH_ASSOC);
-    	$row = $reqette->rowCount();
+	//public function connexion($identifiant, $motDePasse){
+	//	$reqette = $this->bd->prepare("SELECT id_utilisateur, mdp from utilisateur where identifiant = ':identifiant'");
+	//	$reqette->bindValue(':identifiant', $identifiant);
+	//	$reqette->execute();
+	//	$tab = $reqette->fetch(PDO::FETCH_ASSOC);
+	//	$row = $reqette->rowCount();
 
-    	if ($row == 1){
-        	$motDePasseHash = crypt($motDePasse , "md5");
-        	if(password_verify($tab["mdp"] , $motDePasseHash)){
-            	return true;
-        	}
-    	return false;
-    	}
+	//	if ($row == 1){
+	//    	$motDePasseHash = crypt($motDePasse , "md5");
+	//    	if(password_verify($tab["mdp"] , $motDePasseHash)){
+	//        	return true;
+	//    	}
+	//	return false;
+	//	}
+	//}
+
+
+	//DEBUT GETTERS//
+		//DEBUT GET UTILISATEUR//
+	public function getMotDePasse($identifiant){
+		$requette = $this->bd->prepare("SELECT mdp from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
 	}
+	public function getIdentifiant($nom){
+		$requette = $this->bd->prepare("SELECT id_utilisateur from utilisateur where nom = :nom");
+		$requette->bindValue('nom', $nom);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getNom($identifiant){
+		$requette = $this->bd->prepare("SELECT nom from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getPrenom($identifiant){
+		$requette = $this->bd->prepare("SELECT prenom from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getMail($identifiant){
+		$requette = $this->bd->prepare("SELECT mail from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getRole($identifiant){
+		$requette = $this->bd->prepare("SELECT role from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getDateCreation($identifiant){
+		$requette = $this->bd->prepare("SELECT date_creation from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getDateConnexion($identifiant){
+		$requette = $this->bd->prepare("SELECT date_connexion from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getPointsFid($identifiant){
+		$requette = $this->bd->prepare("SELECT point_fid from utilisateur where id_utilisateur = :identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+		//FIN GET UTILISATEUR//
 
+		//DEBUT GET ARTICLE//
+	public function getNomArticle($idArticle){
+		$requette = $this->bd->prepare("SELECT nom_article from article where id_article = :identifiant");
+		$requette->bindValue('identifiant', $idArticle);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getPrix($idArticle){
+		$requette = $this->bd->prepare("SELECT prix from article where id_article = :identifiant");
+		$requette->bindValue('identifiant', $idArticle);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getCategorie($idArticle){
+		$requette = $this->bd->prepare("SELECT categorie from article where id_article = :identifiant");
+		$requette->bindValue('identifiant', $idArticle);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getInfo($idArticle){
+		$requette = $this->bd->prepare("SELECT informations from article where id_article = :identifiant");
+		$requette->bindValue('identifiant', $idArticle);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+	public function getNbArticle($idArticle){
+		$requette = $this->bd->prepare("SELECT nb_article from article where id_article = :identifiant");
+		$requette->bindValue('identifiant', $idArticle);
+		$requette->execute();
+		$tableau = $requette->fetch(PDO::FETCH_NUM);
+		return $tableau[0];
+	}
+		//FIN GET ARTICLE//
+
+	// FIN DES GETTERS//
 	public function consulterInventaire(){
 		$requette = $this->bd->prepare("SELECT * from article");
 		$requette->execute();
 		return $requette->fetchall();;
 	}
-	/**
- 	* cette fonction permet de calculer le montant des achats d’articles et retourne son prix total.
- 	* return int
- 	*/
-	public function calculAchat(){
-  	$reqette = $this->bd->prepare("SELECT SUM(prix) from article  ");
-  	$reqette->execute();
-  	$tab = $reqette->fetch(PDO::FETCH_NUM);
-  	return $tab['0'];
-  	}
 
+	/**
+	 * @return mixed
+	 */
+	public function calculAchat(){
+  		$reqette = $this->bd->prepare("SELECT SUM(prix) from article  ");
+  		$reqette->execute();
+  		$tab = $reqette->fetch(PDO::FETCH_NUM);
+  		return $tab['0'];
+  	}
 
 	public function enregistrementAchats($achats) {
     	// Préparez la requête d'insertion
     	//$stmt = $conn->prepare("INSERT INTO achats (nom, prix, quantite, date) VALUES ($_GET['nom'], $_GET['nom'], $_GET['nom'], $_GET['nom'])");
-    	$stmt->bind_param("sdss", $nom, $prix, $quantite, $date);
+    	//$stmt->bind_param("sdss", $nom, $prix, $quantite, $date);
 
     	// Affectez les valeurs aux paramètres
-    	$nom = $achats->nom;
-    	$prix = $achats->prix;
-    	$quantite = $achats->quantite;
-    	$date = $achats->date;
+    	//$nom = $achats->nom;
+    	//$prix = $achats->prix;
+    	//$quantite = $achats->quantite;
+    	//$date = $achats->date;
 
     	// Exécutez la requête
-    	$stmt->execute();
+    	//$stmt->execute();
 
     	// Fermez la connexion à la base de données
-    	$conn->close();
+    	//$conn->close();
 }
 
-
-
 	public function maxAchat(){
-
+		return null;
 	}
 
 	public function consulterHistorique($id_utilisateur){
-		$requette = $this->bd->prepare("SELECT * from historique_commande join utilisateur on  utilisateur.id_utilisateur = :id_utilisateur  ");
+		$requette = $this->bd->prepare("SELECT * from historique_commande_util join utilisateur on  utilisateur.id_utilisateur = :id_utilisateur  ");
 		$requette->bindValue(':id_utilisateur',$id_utilisateur);
 		$requette->execute();
-		return $requette->fetchall();
+		return $requette->fetchall(PDO::FETCH_ASSOC);
+	}
 
+	public function consulterHistoriqueVentes(){
+		$requete = $this->bd->prepare("SELECT * FROM article JOIN historique_commande ON article.id_article = historique_commande.id_article ORDER BY date_achat DESC");
+		$requete->execute();
+		return $requete->fetchAll();
+	}
+
+	public function consulterBilan(){
+		$requete = $this->bd->prepare("SELECT * FROM article JOIN historique_commande ON article.id_article = historique_commande.id_article WHERE TO_CHAR(date_achat,'mm')=TO_CHAR(CURRENT_DATE,'mm') ORDER BY heure_achat DESC;");
+		$requete->execute();
+		return $requete->fetchAll();
 	}
 
 	public function consulerPariteAchat(){
 		$requette = $this->bd->prepare("SELECT * from historique_commande order by heure_achat,date_achat DESC LIMIT 20 ");
-  	$requette->execute();
-  	return $requette->fetchall();
-
+  		$requette->execute();
+  		return $requette->fetchall();
 	}
 
 	/**
@@ -110,8 +224,8 @@ class Model {
 	public function estEnStock($article){
 		$reqette = $this->bd->prepare("SELECT nb_article from article where id_article = :id_article  ");
 		$reqette->bindValue(':id_article',$article);
-  	$reqette->execute();
-  	$tab = $reqette->fetch(PDO::FETCH_NUM);
+  		$reqette->execute();
+  		$tab = $reqette->fetch(PDO::FETCH_NUM);
 		if ($tab[0] > 0){
 			echo '<p>'. " l'article numero " . $article . ' est encore en stock ' . "</p>";
 		}
@@ -121,39 +235,41 @@ class Model {
 }
 
 
-public function ajouterArticle($donnee){
-       $req = $this->bd->prepare("INSERT INTO article(id_article,nom_article,prix,categorie,informations,nb_article) VALUES (:id_article,:nom_article,:prix,:categorie,:informations,:nb_article)");
-       $marks = ['id_article','nom_article','prix','categorie','informations','nb_article'];
-        foreach ($marks as $value) {
-            $req->bindValue(':' . $value, $donnee[$value]);
-        }
-        $req->execute();
-        return (bool) $req->rowCount();
-    }
+	public function ajouterArticle($donnee){
+       	$req = $this->bd->prepare("INSERT INTO article(id_article,nom_article,prix,categorie,informations,nb_article) VALUES (:id_article,:nom_article,:prix,:categorie,:informations,:nb_article)");
+       	$marks = ['id_article','nom_article','prix','categorie','informations','nb_article'];
+        	foreach ($marks as $value) {
+            	$req->bindValue(':' . $value, $donnee[$value]);
+        	}
+        	$req->execute();
+        	return (bool) $req->rowCount();
+    	}
 
-		public function ajouterArticle2($id_article,$nom_article,$prix,$informations){
-		       $requette = $this->bd->prepare("INSERT INTO article(id_article,nom_article,prix,informations) VALUES (:id_article,:nom_article,:prix,:informations)");
-		       $requette->execute(array(
-		           'id_article' => $id_article,
-		           'nom_article' => $nom_article,
-		           'prix' => $prix,
-		           'informations' => $informations));}
+	public function ajouterArticle2($id_article,$nom_article,$prix,$informations){
+		$requette = $this->bd->prepare("INSERT INTO article(id_article,nom_article,prix,informations) VALUES (:id_article,:nom_article,:prix,:informations)");
+		$requette->execute(array(
+			'id_article' => $id_article,
+			'nom_article' => $nom_article,
+			'prix' => $prix,
+			'informations' => $informations));}
+
 
 	public function reduction(){
     	return null;
 	}
 
-	public function ajouterCompte($identifiant, $nom, $prenom, $mail, $motDePasse){
-    	$requette = $this->bd->prepare("INSERT INTO utilisateur(identifiant, nom, prenom, mail, mdp, role) VALUES (:identifiant , :nom , :prenom, :mail, :motDePasse, :role)");
+	public function ajouterCompte($identifiant, $nom, $prenom, $mail, $motDePasse, $pf = 5){
+    	$requette = $this->bd->prepare("INSERT INTO utilisateur(id_utilisateur, nom, prenom, mail, mdp, role, point_fid ) VALUES (:identifiant , :nom , :prenom, :mail, :motDePasse, :role, :pointsFidel)");
     	$role = 'utilisateur';
     	$motDePasseHash = crypt($motDePasse, 'md5');
     	$requette->execute(array(
-        	'identifiant' => $identifiant ,
-        	'nom' => $nom ,
-        	'prenom' => $prenom ,
-        	'mail' => $mail,
-        	'motDePasse' => $motDePasseHash,
-        	'role' => $role));
+			'identifiant' => $identifiant ,
+			'nom' => $nom ,
+			'prenom' => $prenom ,
+			'mail' => $mail,
+			'motDePasse' => $motDePasseHash,
+			'role' => $role,
+			'pointsFidel' => $pf));
 	}
 
 	/**
@@ -165,40 +281,53 @@ public function ajouterArticle($donnee){
 	 * @param $motDePasse
 	 * @return void
 	 */
-	public function ajoutCompteAdministrateur($identifiant, $nom, $prenom, $mail, $motDePasse){
+	public function ajoutCompteAdministrateur($identifiant, $nom, $prenom, $mail, $motDePasse, $pf = 5){
     	$role = 'administrateur';
     	$motDePasseHash = crypt($motDePasse, 'md5');
-    	$requette = $this->bd->prepare("INSERT INTO utilisateur(id_utilisateur, nom, prenom, mail, mdp, role ) VALUES (:identifiant , :nom , :prenom, :mail, :motDePasse, :role)");
+    	$requette = $this->bd->prepare("INSERT INTO utilisateur(id_utilisateur, nom, prenom, mail, mdp, role, point_fid ) VALUES (:identifiant , :nom , :prenom, :mail, :motDePasse, :role, :pointsFidel)");
     	$requette->execute(array(
         	'identifiant' => $identifiant ,
         	'nom' => $nom ,
         	'prenom' => $prenom ,
         	'mail' => $mail,
         	'motDePasse' => $motDePasseHash,
-        	'role' => $role));
+        	'role' => $role,
+			'pointsFidel' => $pf));
 	}
 
 	public function infoCompte($id_utilisateur){
-    $requette = $this->bd->prepare("SELECT * from client where id_utilisateur = :id_utilisateur");
-		$reqette->bindValue(':id_utilisateur',$id_utilisateur);
-    $requette->execute();
-    return $requette->fetchall();
+    	$requette = $this->bd->prepare("SELECT * from client where id_utilisateur = :id_utilisateur");
+		$requette->bindValue(':id_utilisateur',$id_utilisateur);
+    	$requette->execute();
+    	return $requette->fetchall();
 	}
 
 	public function supprimerCompte($identifiant){
-    	return null;
+    	$requette = $this->bd->prepare("DELETE FROM utilisateur where id_utilisateur=:identifiant");
+		$requette->bindValue('identifiant', $identifiant);
+		$requette->execute();
 	}
 
 	public function supprimerArticle($article){
-    	return null;
+		$requete = $this->bd->prepare("DELETE FROM article where id_article=:article");
+		$requete->bindValue('article', $article);
+		$requete->execute();
 	}
 
-	public function deleguerCompteAdmin($identifiant){
-    	return null;
+	public function deleguerCompteAdmin($idadmin,$iduser){
+		$requete1 = $this->bd->prepare("UPDATE utilisateur SET role = 'administrateur' WHERE id_utilisateur=:iduser");
+		$requete1->bindValue('iduser', $iduser);
+		$requete1->execute();
+		$requete2 = $this->bd->prepare("UPDATE utilisateur SET role = 'utilisateur' WHERE id_utilisateur=:idadmin");
+		$requete2->bindValue('idadmin', $idadmin);
+		$requete2->execute();
 	}
 
-	public function rapprovisionnement($idArticle, $qte){
-    	return null;
+	public function reapprovisionnement($idArticle, $qte){
+		$requete = $this->bd->prepare("update article set nb_article = nb_article + :qte where id_article = :idArticle");
+		$requete->bindValue('idArticle', $idArticle);
+		$requete->bindValue('qte', $qte);
+		$requete->execute();
 	}
 
 	public function envoiNotification(){
@@ -208,12 +337,5 @@ public function ajouterArticle($donnee){
 	public function fixerStockBas($idArticle, $minimum){
     	return null;
 	}
-
-	public function getHistorique(){
-		$req = $this->bd->prepare('SELECT * FROM historique_commande ORDER BY date_achat DESC');
-		$req->execute();
-		return $req->fetchall();
-	}
-
 }
 ?>
