85ea77f40b0ee10f428c947e17683552d219ce96
Andriamisa Nayann <99319983+nayannou@users.noreply.github.com>
2023-01-15 19:31:22 +0100

    Add files via upload

--- View/view_login.php
+++ View/view_login.php
@@ -1,19 +1,38 @@
-<?php ?>
 <!Doctype html>
 <html>
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../Content/CSS/style.css">
+    <link rel="shortcut icon" href="../Content/Images/Logo_bde">
+    <title>BDE USPN</title>
 </head>
-
 <body>
-<div style="border-radius: 12px; border: solid 1px black; text-align: center; padding: 10% 10%; width: 50%; margin: auto; margin-top: 10% ">
-    <form action="" method="post">
-        <label>Identifiant : <input name="identifiant" type="number"></label><br>
-        <label>Mot de passe : <input name="mdp" type="password"></label><br>
-        <input type="submit" value="LOGIN" style="margin: 15px"><br>
-        <span>Pas encore un compte ? <a href="?controller=identification&action=signin">Créer un compte</a></span>
-    </form>
-</div>
-</body>
 
-</html>
+<div class="container" id="container_login">
+    <div class="logo" id="logo_login">
+        <a href="?controller=accueil&action_membre">
+            <img src="../Content/Images/Logo_BDE_noir.png" alt="Logo BDE" style="height: 100px;width: 100px">
+        </a>
+    </div>
+</div>
+
+
+<section class="accueil" id="accueil_form">
+    <div class="identification">
+        <h1 class="login_title">CONNEXION</h1>
+        <form action="" method="post" class="formulaire">
+            <label>Identifiant :</label>
+            <br>
+            <input name="identifiant" type="number" class="input_login">
+            <br>
+            <label>Mot de passe :</label>
+            <br>
+            <input name="mdp" type="password" class="input_login">
+            <br>
+            <input type="submit" value="LOGIN" class="submit_login"><br>
+            <span>Pas encore de compte ? <a href="?controller=identification&action=signin">Créer un compte</a></span>
+        </form>
+    </div>
+</section>
+
+<?php require_once "view_end.php" ?>
