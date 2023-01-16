1fd77b27eba6952d330b22c4fc4cb00b2af422ec
Andriamisa Nayann <99319983+nayannou@users.noreply.github.com>
2023-01-16 13:50:51 +0100

    Add files via upload

--- Controller/Controller_article.php
+++ Controller/Controller_article.php
@@ -1,6 +1,28 @@
 <?php
 class Controller_article extends Controller{
 
+    public function action_snack(){
+        $m = Model::getModel();
+        $data = $m->getSnack();
+        $this->render("commande", $data);
+    }
+
+    public function action_boisson(){
+        $m = Model::getModel();
+        $data = $m->getBoisson();
+        $this->render("commande", $data);
+    }
+
+    public function action_soda(){
+        $m = Model::getModel();
+        $data = $m->getSoda();
+        $this->render("commande", $data);
+    }
+
+    public function action_default(){
+        $this->action_snack();
+    }
+     
 }
 
 ?>
--- Controller/Controller_historique.php
+++ Controller/Controller_historique.php
@@ -3,13 +3,9 @@
 class Controller_historique extends Controller{
 
     public function action_client(){
-        if (isset($_GET["id"])) {
             $m = Model::getModel();
             $data = $m->consulterHistorique($_GET["id"]);
             $this->render("historique_client", $data);
-        } else {
-            $this->action_error("Vous n'êtes pas connecté.\nVeuillez vous connecter pour accéder à votre historique");
-        }
     }
 
     public function action_ventes(){
@@ -17,6 +13,7 @@ class Controller_historique extends Controller{
         $data = $m->consulterHistoriqueVentes();
         $this->render("bilan", $data);
     }
+
     public function action_bilan()
     {
         $m = Model::getModel();
@@ -25,12 +22,7 @@ class Controller_historique extends Controller{
     }
 
     public function action_default(){
-        $m = Model::getModel();
-        if ($m->getRole($_GET["id"]) = "administrateur"){
-            $this->action_bilan();
-        } else {
-            $this->action_client();
-        }
+    /* à faire */
     }
 }
 ?>
